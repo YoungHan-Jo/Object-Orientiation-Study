fa226a2234eb8554ae08192d7f27d0ebdeda085f
Jo YoungHan <verin4494@gmail.com>
2022-12-09 12:41:45 +0900

    java object start

---
+++ javaObject/src/main/java/hello/javaObject/JavaObjectApplication.java
@@ -0,0 +1,13 @@
+package hello.javaObject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaObjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaObjectApplication.class, args);
+	}
+
+}
