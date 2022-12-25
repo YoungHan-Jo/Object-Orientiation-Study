eb854858d0a1be9c8fe4ffd664651c79cfb45f07
Jo YoungHan <verin4494@gmail.com>
2022-12-25 13:43:34 +0900

    ex7_14

---
+++ javaObject/src/main/java/hello/javaObject/Ex7_14.java
@@ -0,0 +1,30 @@
+package hello.javaObject;
+
+public class Ex7_14 {
+    private int outerIv = 0;
+    static int outerCv = 0;
+
+    class InstanceInner {
+        int iiv = outerIv;
+        int iiv2 = outerCv;
+    }
+
+    static class StaticInner {
+//        int siv = outerIv;
+        static int scv2 = outerCv;
+    }
+
+    void myMethod() {
+        int lv = 0;
+        final int LV = 0;
+
+        class LocalInner {
+            int liv = outerIv;
+            int liv2 = outerCv;
+            int liv3 = lv;
+            int liv4 = LV;
+        }
+    }
+
+
+}
